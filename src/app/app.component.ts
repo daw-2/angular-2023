a407cd20c6925651b839f3eabb1a81a34d7ea5e6
Matthieu Mota <matthieumota@gmail.com>
2023-09-11 14:48:41 +0200

    Display a pizza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pizza } from './models/pizza';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = 'Pizza Party';
   name: string = '4 fromages';
+  pizza: Pizza = {
+    id: 1,
+    name: '4 fromages',
+    price: 4.99,
+    image: '4-fromages.jpg',
+  };
+  pizza2: Pizza = new Pizza(2, 'Reine', 5.99);
+
+  switchPizza(): void {
+    let tmp = this.pizza;
+    this.pizza = this.pizza2;
+    this.pizza2 = tmp;
+  }
 }
