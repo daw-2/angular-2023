f4b348d4da959c3e94b988f7227a5c159d8cd07d
Matthieu Mota <matthieumota@gmail.com>
2023-09-13 15:40:41 +0200

    Env file for deployment

--- src/app/services/pizza.service.ts
+++ src/app/services/pizza.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import { Pizza } from '../models/pizza';
 import { Observable, delay, map, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PizzaService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    console.log(environment.name);
+  }
 
   getPizzas(): Observable<Pizza[]> {
-    return this.http.get<Pizza[]>('http://localhost:3000/pizzas')
+    return this.http.get<Pizza[]>(environment.apiUrl+'/pizzas')
       .pipe(
         // Transformer le tableau brut en tableau de Pizzas (Map de l'API avec le modèle)
         map((data: any[]) => data.map((item: any) => new Pizza(item.id, item.name, item.price, item.image))),
@@ -19,7 +22,7 @@ export class PizzaService {
   }
 
   getPizza(id: number): Observable<Pizza> {
-    return this.http.get<Pizza>('http://localhost:3000/pizzas/'+id)
+    return this.http.get<Pizza>(environment.apiUrl+'/pizzas/'+id)
       .pipe(
         map((data: any) => new Pizza(data.id, data.name, data.price, data.image)),
         delay(500)
@@ -27,7 +30,7 @@ export class PizzaService {
   }
 
   exists(name: string, exclude: number): Observable<boolean> {
-    return this.http.get<any[]>('http://localhost:3000/pizzas?name='+name+'&id_ne='+exclude).pipe(
+    return this.http.get<any[]>(environment.apiUrl+'/pizzas?name='+name+'&id_ne='+exclude).pipe(
       map((data: any[]) => data.length > 0),
     );
   }
