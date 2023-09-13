88e807649af35cf4dd10c04a3e342818a5a4ab37
Matthieu Mota <matthieumota@gmail.com>
2023-09-13 20:38:41 +0200

    Fix test

--- src/app/pages/pizzas/pizzas.component.spec.ts
+++ src/app/pages/pizzas/pizzas.component.spec.ts
@@ -1,6 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, tick, waitForAsync } from '@angular/core/testing';
 
 import { PizzasComponent } from './pizzas.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { IngredientListComponent } from 'src/app/ingredient-list/ingredient-list.component';
+import { PizzaComponent } from 'src/app/pizza/pizza.component';
+import { AppRoutingModule } from 'src/app/modules/app-routing/app-routing.module';
 
 describe('PizzasComponent', () => {
   let component: PizzasComponent;
@@ -8,14 +13,23 @@ describe('PizzasComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PizzasComponent]
+      imports: [HttpClientModule, AppRoutingModule],
+      declarations: [PizzasComponent, PizzaComponent, IngredientListComponent]
     });
     fixture = TestBed.createComponent(PizzasComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create', waitForAsync(() => {
     expect(component).toBeTruthy();
-  });
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      const pizzas = fixture.nativeElement.querySelectorAll('ul.pizzas li');
+      expect(pizzas.length).toBe(4);
+      expect(pizzas[0].textContent.trim()).toContain('0: Reine');
+    })
+  }));
 });
