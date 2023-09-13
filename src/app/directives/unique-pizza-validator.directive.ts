f60016292dc418180f1950efd68264f7f855c696
Matthieu Mota <matthieumota@gmail.com>
2023-09-13 20:27:26 +0200

    Fix validator

--- src/app/directives/unique-pizza-validator.directive.ts
+++ src/app/directives/unique-pizza-validator.directive.ts
@@ -1,6 +1,6 @@
 import { Directive } from '@angular/core';
 import { AbstractControl, AsyncValidator, NG_ASYNC_VALIDATORS, ValidationErrors } from '@angular/forms';
-import { Observable, debounceTime, delay, map, of, switchMap } from 'rxjs';
+import { Observable, debounceTime, delay, first, map, of, switchMap } from 'rxjs';
 import { PizzaService } from '../services/pizza.service';
 
 @Directive({
@@ -13,6 +13,11 @@ import { PizzaService } from '../services/pizza.service';
   }]
 })
 export class UniquePizzaValidatorDirective implements AsyncValidator {
+  private lastError: any = {
+    value: null,
+    result: null,
+  };
+
   constructor(private pizzaService: PizzaService) {}
 
   validate(control: AbstractControl<any, any>): Observable<ValidationErrors | null> {
@@ -20,12 +25,24 @@ export class UniquePizzaValidatorDirective implements AsyncValidator {
       return of(null);
     }
 
+    if (this.lastError.value === control.value) {
+      return of(this.lastError.result);
+    }    
+
     // Optimisation sur l'API
-    // return control.valueChanges.pipe(
-    //   debounceTime(500),
-    //   switchMap(v => this.pizzaService.exists(v, 2)),
-    //   map((exists: boolean) => exists ? { appUniquePizza: true } : null),
-    // );
+    return control.valueChanges.pipe(
+      debounceTime(500),
+      switchMap(v => {
+        this.lastError.value = control.value;
+        return this.pizzaService.exists(v, 2)
+      }),
+      map((exists: boolean) => {
+        this.lastError.result = exists ? { appUniquePizza: true } : null
+
+        return this.lastError.result;
+      }),
+      first(),
+    );
 
     return this.pizzaService.exists(control.value, 2).pipe(
       map((exists: boolean) => exists ? { appUniquePizza: true } : null),
